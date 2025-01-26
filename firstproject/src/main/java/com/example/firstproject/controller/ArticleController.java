2f8405574f0648a718d83753b532c6e0d62c89ab
YoungHyeon1 <73692488+YoungHyeon1@users.noreply.github.com>
2025-01-26 21:07:24 +0900

    add spring project

---
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -0,0 +1,29 @@
+package com.example.firstproject.controller;
+
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ArticleController {
+    @Autowired // Dependency Injection
+    private ArticleRepository articleRepository;
+
+    @GetMapping("/articles/new")
+    public String newArticleForm() {
+        return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm articleForm) {
+        Article article = articleForm.toEntity();
+        Article saved = articleRepository.save(article);
+        System.out.println(saved.toString());
+        return "";
+    }
+}
